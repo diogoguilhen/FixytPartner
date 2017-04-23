44945c9d2e0352a654759629222adad81b2da3db
Diogo Henrique S Guilhen <diogo.guilhen@gmail.com>
2017-04-22 22:43:14 -0300

    novo Layout do Drawer

--- app/src/main/java/fixyt/fixytMotor/Main.java
+++ app/src/main/java/fixyt/fixytMotor/Main.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Main extends AppCompatActivity implements View.OnClickListener{
